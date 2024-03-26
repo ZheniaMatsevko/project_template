f800e193af592b024f06337eb4d214aa7c74b7c8
Yevheniia Matsevko <mazevkozhenia@gmail.com>
2024-03-26 10:47:16 +0200

    task 4. add empty functions to input.py and output.py

---
+++ app/io/input.py
@@ -0,0 +1,40 @@
+
+
+def read_from_console(prompt):
+    """
+    Reads a user console input using a standard function.
+
+    Args:
+        prompt (str): Used to tell a user what input is expected.
+
+    Returns: str. User input.
+    """
+    pass
+
+
+def read_from_file(filepath):
+    """
+    Reads a file content by a given name.
+
+    Args:
+        filepath (str): The path to the file to read from.
+
+    Returns:
+        str. Contents of the requested file.
+        None. If there is no such file or directory.
+    """
+    pass
+
+
+def read_from_file_with_pandas(filepath):
+    """
+       Reads a file content by a given name using pandas.
+
+       Args:
+           filepath (str): The path to the file to read from.
+
+       Returns:
+           str. Contents of the requested file.
+           None. If there is no such file or directory.
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,25 @@
+
+
+def write_to_console(text):
+    """
+    Writes to console a given text using standard function.
+
+    Args:
+        text (str): The text to be printed in console.
+    """
+    pass
+
+
+def write_to_file(contents, filepath):
+    """
+    Writes contents to a file by its path.
+
+    Note: if a file by filepath already exists, it will be overwritten!
+
+    Args:
+        contents (str): Text to be writen to a file.
+        filepath (str): A path to a file
+
+    Returns: Bool. True, if content are successfully written.
+    """
+    pass
